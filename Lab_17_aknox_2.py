641b59a6d14df3cd79cf77118d067cfba6640e97
AKnox12 <aknox12@student.cscc.edu>
2025-05-09 20:53:17 -0400

    Added an other file with the plotting of language changes. Added a html file. Rewrote the title to font to be recognise in the program.

--- Lab_17_aknox_2.py
+++ Lab_17_aknox_2.py
@@ -3,3 +3,10 @@
 # Plotting other language repos
 # May 7th 2025
 
+from cpp_repos_visual import Cpp_repo_visualizer
+
+def main():
+    my_visiualizer = Cpp_repo_visualizer.Viaualize()
+
+if __name__ == "__main__":
+    main()
